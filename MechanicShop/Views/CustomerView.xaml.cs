9c4f246b40c608004fd99f5012c529afec2bcfcf
Chloe Nibali <Chloe.Nibali@edu.sait.ca>
2024-03-29 17:27:44 -0600

    Stylization. Customer page framework in place

--- MechanicShop/Views/CustomerView.xaml.cs
+++ MechanicShop/Views/CustomerView.xaml.cs
@@ -10,10 +10,48 @@ public partial class CustomerView : ContentPage
 
     private void clearCustomerSearch_Clicked(object sender, EventArgs e)
     {
-
+        // inactive unless searh fields are filled in
+        // clears search form
     }
 
     private void searchCustoemrsBtn_Clicked(object sender, EventArgs e)
+    {
+        // inactive unless searfh fields are filled in
+        // Pairs down collectionview to only items matching search terms
+    }
+
+    private void deleteCustomer_Clicked(object sender, EventArgs e)
+    {
+        // make inactive unless vehicle is selected
+        // Confirmation message to prevent accidental detetion
+    }
+
+    private void updateCustomerInfo_Clicked(object sender, EventArgs e)
+    {
+        // Modal view? update info or cancel
+    }
+
+    private void addVehicle_Clicked(object sender, EventArgs e)
+    {
+        // Add in a popup???
+        // Can we use pickers with pre loaded makes and models in a popup???
+        // Look into MODAL NAVIAGATION pages!
+    }
+
+    private void editVehicle_Clicked(object sender, EventArgs e)
+    {
+        // make inactive unless vehicle is selected
+        // Edit in a popup???
+        // Give option to cancel!!!
+    }
+
+    private void deleteVehicle_Clicked(object sender, EventArgs e)
+    {
+        // make inactive unless vehicle is selected
+        // Confirmation message to prevent accidental detetion
+    }
+
+    private void newAppointmentBtn_Clicked(object sender, EventArgs e)
     {
 
     }
---
+++ MechanicShop/Views/TechView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MechanicShop.Views;
+
+public partial class TechView : ContentPage
+{
+	public TechView()
+	{
+		InitializeComponent();
+	}
+}
