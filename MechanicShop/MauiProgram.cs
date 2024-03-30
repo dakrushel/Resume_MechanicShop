d0c61ab6688b78ea8ff6de1f82ac264e0bb98c63
Chloe Nibali <Chloe.Nibali@edu.sait.ca>
2024-03-30 16:04:54 -0600

    Can now fully CRUD customers

--- MechanicShop/MauiProgram.cs
+++ MechanicShop/MauiProgram.cs
@@ -5,6 +5,7 @@ namespace MechanicShop
 {
     public static class MauiProgram
     {
+        public static MechanicShopSQLite ShopDB;
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -21,7 +22,7 @@ namespace MechanicShop
             Customer Jane = new Customer("Jane", "124 Avenue", "999-000-9999");
 
 
-            MechanicShopSQLite mechanicShopDB = new MechanicShopSQLite();
+            ShopDB = new MechanicShopSQLite();
 
             
 
