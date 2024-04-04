060b001b3b36733faf19f21929c1d3cfa1eb5c84
Chloe Nibali <Chloe.Nibali@edu.sait.ca>
2024-04-03 20:58:05 -0600

    Can create new appointment

--- MechanicShop/Models/RepairJobServiceJobBridge.cs
+++ MechanicShop/Models/RepairJobServiceJobBridge.cs
@@ -1,6 +1,7 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,13 @@ namespace MechanicShop.Models
 {
     public class RepairOrderServiceJobBridge
     {
-        [PrimaryKey, NotNull]
-        public int RepairJobServiceJobBridgeId {  get; set; }
-
+        //[PrimaryKey, NotNull]
+        //public int RepairJobServiceJobBridgeId {  get; set; }
+        [Required]
+        [PrimaryKey]
         public int RepairOrderId { get; set; }
-
+        [Required]
+        [PrimaryKey]
         public int ServiceJobId { get; set; }
 
         //Opens connection to Database
@@ -21,13 +24,13 @@ namespace MechanicShop.Models
 
         public RepairOrderServiceJobBridge(int repairOrderId, int serviceJobId)
         {
-            Random random = new Random();
+            //Random random = new Random();
 
-            int RepairJobServiceJobBridgeId = random.Next(1000);
-            while (ShopDB.GetAllRepairOrderServiceJobBridge().FirstOrDefault(x => x.RepairOrderId == repairOrderId) != default)
-            {
-                repairOrderId = random.Next(1000);
-            }
+            //int RepairJobServiceJobBridgeId = random.Next(1000);
+            //while (ShopDB.GetAllRepairOrderServiceJobBridge().FirstOrDefault(x => x.RepairOrderId == repairOrderId) != default)
+            //{
+            //    repairOrderId = random.Next(1000);
+            //}
             this.RepairOrderId = repairOrderId;
             this.ServiceJobId = serviceJobId;
         }
--- MechanicShop/Views/AppointmentView.xaml.cs
+++ MechanicShop/Views/AppointmentView.xaml.cs
@@ -1,5 +1,6 @@
 using MechanicShop.Services;
 using MechanicShop.Models;
+using System.Collections.ObjectModel;
 namespace MechanicShop.Views;
 
 
@@ -9,14 +10,17 @@ public partial class AppointmentView : ContentPage
 	public static string TodayDate = currentDate.ToString("yyyy-MM-dd");
 	public static Customer? c = null;
 	public static Vehicle? v = null;
-    public static RepairOrder? repairOrder = null;
+    public static RepairOrder? repairOrder = null;   
+    public  static ObservableCollection<ServiceJob> thisJobs = new ObservableCollection<ServiceJob>();
 
 	public AppointmentView()
 	{
 		InitializeComponent();
 		DateDisplay.Text = TodayDate;
 
-	}
+        var allJobs = new ObservableCollection<ServiceJob>(MauiProgram.ShopDB.GetAllServiceJobs());
+        serviceJobs.ItemsSource = allJobs;
+    }
     protected override void OnAppearing()
     {
         // override initialize to get changes any time the page loads, rather than only
@@ -24,20 +28,32 @@ public partial class AppointmentView : ContentPage
         base.OnAppearing();
         if (Pass.CustomerPass != null && Pass.VehiclePass != null)
         {
+            //widgets
             appointmentSlip.IsEnabled = true;
             appointmentLogs.IsEnabled = false;
-            c = Pass.RetreiveCustomer();
-            cInformation.BindingContext = c;
+
+            // page temp objects
+            c = Pass.RetreiveCustomer();         
             v = Pass.RetrieveVehicle();
+
+            //Binding Contexts
+            cInformation.BindingContext = c;
             vInformation.BindingContext = v;           
         }
         else 
         { 
+            // page temp objects
             cInformation.BindingContext = null;
             vInformation.BindingContext = null;
+            thisJobs.Clear();
+
+            // Widgets
             appointmentSlip.IsEnabled = false;
+            appointmentLogs.IsVisible = true;
             appointmentLogs.IsEnabled = true;
+            serviceJobMenu.IsVisible = false;
         }
+        datePicker.Date = currentDate;
         
         
         
@@ -47,14 +63,104 @@ public partial class AppointmentView : ContentPage
     {
         
     }
+    //======================================================================================
+    //+++APPOINTMENT SLIP+++
+    //======================================================================================
 
     private void removeJobBtn_Clicked(object sender, EventArgs e)
     {
-
+        ServiceJob? toRemove = repairOrderJobs.SelectedItem as ServiceJob;
+        if (toRemove != null)
+        {
+            thisJobs.Remove(toRemove);
+            RefreshROJobs();
+        }     
+        removeJobBtn.IsEnabled = false;
     }
 
     private void addJobBtn_Clicked(object sender, EventArgs e)
     {
+        appointmentLogs.IsVisible = false;
+        serviceJobMenu.IsVisible = true;
+        addJobBtn.IsEnabled = false;
+    }
+    private void repairOrderJobs_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        ServiceJob? sj = e.SelectedItem as ServiceJob;
+        if (sj != null)
+        {
+            removeJobBtn.IsEnabled = true;
+            removeJobBtn.Text = $"Remove {sj.ServiceJobDescription}";
+        }
+        
+    }
 
+    //======================================================================================
+    //+++Service Job Menu+++
+    //======================================================================================
+    private void serviceJobs_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        ServiceJob? sj = e.SelectedItem as ServiceJob;
+        if (sj != null)
+        {
+            thisJob.BindingContext = sj;
+            if (thisJobs.Contains(sj))
+            {
+                addThisJob.IsEnabled = false;
+                return;
+            }
+            addThisJob.IsEnabled = true;
+        }
+    }
+    private void closeServiceJobMenu_Clicked(object sender, EventArgs e)
+    {
+        appointmentLogs.IsVisible = true;
+        serviceJobMenu.IsVisible = false;
+        addJobBtn.IsEnabled = true;
+    }
+    private void addThisJob_Clicked(object sender, EventArgs e)
+    {
+        ServiceJob? sj = thisJob.BindingContext as ServiceJob;
+        if (sj != null)
+        {
+            if (thisJobs.Contains(sj))
+            {
+                return;
+            }
+            thisJobs.Add(sj);
+            addThisJob.IsEnabled = false;
+            RefreshROJobs();
+        }
+        
+    }
+
+
+
+
+    private void RefreshROJobs()
+    {
+        repairOrderJobs.ItemsSource = thisJobs;
+        removeJobBtn.Text = "Remove Job";
+        removeJobBtn.IsEnabled = false;
+    }
+
+    private void scheduleAppointment_Clicked(object sender, EventArgs e)
+    {
+        //gather information needed for appointment
+        string? description = problemDescriptionEntry.Text;
+        string? createDate = TodayDate;
+        string? appointmentDate = datePicker.Date.ToString("yyyy-MM-dd");
+        string? VIN = v?.VIN;
+
+        if (description != null && createDate != null && appointmentDate != null && VIN != null)
+        {
+            RepairOrder newAppointment = new RepairOrder(description, createDate, appointmentDate, VIN);
+            foreach (ServiceJob job in thisJobs)
+            {
+                newAppointment.AssignServiceJob(job.ServiceJobId);
+            }
+            OnAppearing();
+        }
+        
     }
 }
