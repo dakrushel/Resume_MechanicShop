9b3abb2bb5b936d58042f3c93b5be8373f7a2eca
Denver Krushel <Denver.Krushel@edu.sait.ca>
2024-04-05 09:49:54 -0600

    Initial commit.

---
+++ MechanicShop/Models/GenerateInvoice.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicShop.Models
+{
+    public class GenerateInvoice
+    {
+        //Getters and setters
+        public string RepairOrderOutput { get; set; }
+
+        //Ctors
+        public GenerateInvoice() { }
+        //public GenerateInvoice(RepairOrder rO)
+        //{
+        //    //Retrieve RepairOrder (create temp RO?)
+        //    RepairOrder temp = rO;
+        //    this.RepairOrderOutput = 
+        //        $"==================================================================\n" +
+        //        $"               REPAIR ORDER {rO.RepairOrderId}\n" +
+        //        $"    ----------------------------------------------------------\n" +
+        //        $"    Description:\n" +
+        //        $"    {rO.RepairOrderDescription}\n" +
+        //        $"    ----------------------------------------------------------\n" +
+        //        $"    \t\t\tDate\n\n" +
+        //        $"    Created:\t     Appointment:\t   Closed:\n" +
+        //        $"    {rO.DateCreated}         {rO.AppointmentDate}\t   {rO.DateClose}\n\n" +
+        //        $"    ----------------------------------------------------------\n" +
+        //        $"     Technician: {rO.RepairJobTechnician}\n" +
+        //        $"    Employee ID: {rO.EmployeeId}\n" +
+        //        $"         Job ID: {rO.ServiceJobId}\n" +
+        //        $"          Hours: {rO.RepairOrderHours}\n" +
+        //        $"        Vehicle: {rO.Vehicle}\r\n" +
+        //        $"            VIN: {rO.VIN}\n\n";
+        //}
+
+        public void SaveInvoiceDelRO(RepairOrder rO)
+        {
+            this.RepairOrderOutput =
+                $"==================================================================\n" +
+                $"               REPAIR ORDER {rO.RepairOrderId}\n" +
+                $"    ----------------------------------------------------------\n" +
+                $"    Description:\n" +
+                $"    {rO.RepairOrderDescription}\n" +
+                $"    ----------------------------------------------------------\n" +
+                $"    \t\t\tDate\n\n" +
+                $"    Created:\t     Appointment:\t   Closed:\n" +
+                $"    {rO.DateCreated}         {rO.AppointmentDate}\t   {rO.DateClose}\n\n" +
+                $"    ----------------------------------------------------------\n" +
+                $"     Technician: {rO.RepairJobTechnician}\n" +
+                $"    Employee ID: {rO.EmployeeId}\n" +
+                $"         Job ID: {rO.ServiceJobId}\n" +
+                $"          Hours: {rO.RepairOrderHours}\n" +
+                $"        Vehicle: {rO.Vehicle}\r\n" +
+                $"            VIN: {rO.VIN}\n\n";
+
+            using (StreamWriter sw = new StreamWriter(Constant.RepairOrderPath))
+            {
+                sw.Write(this.RepairOrderOutput); 
+            }
+        }
+        //Output txt
+
+        //Remove Repair Order from system
+    }
+}
