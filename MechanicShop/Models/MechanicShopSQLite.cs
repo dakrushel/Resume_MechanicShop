259217c473c2352340d1efdb9175b36e36ddf75f
Denver Krushel <Denver.Krushel@edu.sait.ca>
2024-04-10 09:08:33 -0600

    Invoice done?

--- MechanicShop/Models/MechanicShopSQLite.cs
+++ MechanicShop/Models/MechanicShopSQLite.cs
@@ -281,8 +281,6 @@ namespace MechanicShop.Models
             }
         }
 
-
-
         //TESTED
         public void UpdateRepairOrder(RepairOrder repairOrder)
         {
@@ -316,14 +314,25 @@ namespace MechanicShop.Models
             return this.database.Table<RepairOrder>().First(x => x.RepairOrderId == PK);
         }
 
-
-
         public List<RepairOrder> GetRepairOrdersByVIN(string vin) //Denver
         {
             return this.database.Table<RepairOrder>().ToList()
                 .Where(x => x.VIN == vin).ToList();
         }
 
+        public List<RepairOrder> GetRepairOrdersByCustPhone(string customerPhone)
+        {
+            return this.database.Table<RepairOrder>()
+                .Where(x => x.CustomerPhoneNumber.ToLower().Contains(customerPhone.ToLower()))
+                .ToList();
+        }
+
+        public List<RepairOrder> GetRepairOrdersByCustName(string customerName)
+        {
+            return this.database.Table<RepairOrder>()
+                .Where(x => x.CustomerName.ToLower().Contains(customerName.ToLower()))
+                .ToList();
+        }
 
         /*----------------------------SERVICE JOB ------------------------------------*/
 
